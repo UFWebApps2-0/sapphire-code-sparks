701ed98c2780ffb7649f6925486770d9561f6db2
JustinBeauchesne <beauchesnej@ufl.edu>
2023-11-02 13:01:30 -0400

    base imports for App

--- 4b/App.jsx
+++ 4b/App.jsx
@@ -1,1 +1,8 @@
 // Assessment Workspace Core
+import React, {useState} from "react";
+import ElaspedTime from "./elapsedTime";
+import Submission from "./submission";
+function App({data}){
+
+}
+export default App;
--- 4b/submission.jsx
+++ 4b/submission.jsx
@@ -2,4 +2,4 @@
 function Submission(){
 
 }
-export default Submission;
+export default Submission;  
