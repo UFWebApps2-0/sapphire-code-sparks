b70dad3b12cc0895dce29d57696bc0200a65c2eb
rpaigemei <paigereeves@ufl.edu>
2023-11-03 15:00:01 -0400

    saved workspaces -> gallery

--- client/src/views/Gallery/Gallery.jsx
+++ client/src/views/Gallery/Gallery.jsx
@@ -1,15 +1,32 @@
 import React from "react"
 import NavBar from "../../components/NavBar/NavBar"
 import Search from "../../components/Search/Search"
+import GalleryView from "../../components/GalleryView/GalleryView"
 import "./Gallery.less"
+import { useSearchParams } from 'react-router-dom';
+import { useState } from "react";
 
 export default function Gallery(props) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [tab, setTab] = useState(
+    searchParams.has('tab') ? searchParams.get('tab') : 'home'
+  );
+  const [page, setPage] = useState(
+    searchParams.has('page') ? parseInt(searchParams.get('page')) : 1
+  );
+  const [viewing, setViewing] = useState(parseInt(searchParams.get('activity')));
+
   return (
     <div className="container nav-padding">
       <NavBar />
       <div id="about-content-container">
         <p>Public Gallery</p>
         <Search />
+        <GalleryView 
+            searchParams={searchParams}
+            setSearchParams={setSearchParams}
+        />
       </div>
     </div>
   )
