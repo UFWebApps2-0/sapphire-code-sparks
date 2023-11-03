9aaa8eee10c98877f34fa702c622c575afbc42c8
Max Plummer <mplummer1@ufl.edu>
2023-11-03 14:23:37 -0400

    NavBar updated to direct to Gallery/Search page

--- client/src/views/Gallery/Gallery.jsx
+++ client/src/views/Gallery/Gallery.jsx
@@ -1,35 +1,16 @@
 import React from "react"
-<<<<<<< HEAD
-import NSF from "../../assets/nsf_logo.png"
-import TAMU from "../../assets/tamu_logo.png"
-import UF from "../../assets/uf_logo.png"
 import NavBar from "../../components/NavBar/NavBar"
-import "./gallery.less"
+import Search from "../../components/Search/Search"
+import "./Gallery.less"
 
 export default function Gallery(props) {
     return (
         <div className="container nav-padding">
             <NavBar />
             <div id="about-content-container">
-
+                <p>Public Gallery</p>
+                <Search />
             </div>
         </div>
     )
 }
-=======
-import NavBar from "../../components/NavBar/NavBar"
-import Search from "../../components/Search/Search"
-import "./Gallery.less"
-
-export default function Gallery(props) {
-  return (
-    <div className="container nav-padding">
-      <NavBar />
-      <div id="about-content-container">
-        <p>Public Gallery</p>
-        <Search />
-      </div>
-    </div>
-  )
-}
->>>>>>> origin/paige
