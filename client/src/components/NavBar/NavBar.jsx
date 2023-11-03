a7c99726c635bf74918bb1e45ca2ab26df7e8245
Max Plummer <mplummer1@ufl.edu>
2023-11-03 14:07:52 -0400

    something

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -29,102 +29,114 @@ export default function NavBar() {
   };
 
   const menu = (
-    <Menu>
-      {shouldShowRoute('Home') ? (
-        <Menu.Item key='0' onClick={() => handleRouteChange(routes.Home)}>
-          <i className='fa fa-home' />
-          &nbsp; Home
-        </Menu.Item>
-      ) : null}
-      {shouldShowRoute('Dashboard') ? (
-        <Menu.Item key='1' onClick={() => handleRouteChange(routes.Dashboard)}>
-          <i className='fa fa-home' />
-          &nbsp; Dashboard
-        </Menu.Item>
-      ) : null}
-      {shouldShowRoute('ContentCreatorDashboard') ? (
-        <Menu.Item
-          key='2'
-          onClick={() => handleRouteChange(routes.ContentCreatorDashboard)}
-        >
-          <i className='fa fa-home' />
-          &nbsp; Dashboard
-        </Menu.Item>
-      ) : null}
-      {shouldShowRoute('ResearcherDashboard') ? (
-        <Menu.Item
-          key='3'
-          onClick={() => handleRouteChange(routes.ResearcherDashboard)}
-        >
-          <i className='fa fa-home' />
-          &nbsp; Dashboard
-        </Menu.Item>
-      ) : null}
-      {shouldShowRoute('Sandbox') ? (
-        <Menu.Item
-          key='4'
-          onClick={() => {
-            localStorage.removeItem('sandbox-activity');
-            handleRouteChange(routes.Sandbox);
-          }}
-        >
-          <i className='fa fa-window-maximize' />
-          &nbsp; Sandbox
-        </Menu.Item>
-      ) : null}
-      {shouldShowRoute('TeacherLogin') ? (
-        <Menu.Item
-          key='5'
-          onClick={() => handleRouteChange(routes.TeacherLogin)}
-        >
-          <i className='fa fa-sign-in-alt' />
-          &nbsp; User Login
-        </Menu.Item>
-      ) : null}
-      {shouldShowRoute('About') ? (
-        <Menu.Item key='6' onClick={() => handleRouteChange(routes.About)}>
-          <i className='fa fa-info-circle' />
-          &nbsp; About
-        </Menu.Item>
-      ) : null}
-      {shouldShowRoute('BugReport') ? (
-        <Menu.Item key='7' onClick={() => handleRouteChange(routes.BugReport)}>
-          <i className='fa fa-calendar-times' />
-          &nbsp; Report a Bug
-        </Menu.Item>
-      ) : null}
-      {shouldShowRoute('SignOut') ? (
-        <Menu.Item key='8' onClick={() => handleLogout()}>
-          <i className='fa fa-sign-out-alt' />
-          &nbsp; Sign Out
-        </Menu.Item>
-      ) : null}
-    </Menu>
+      <Menu>
+        {shouldShowRoute('Home') ? (
+            <Menu.Item key='0' onClick={() => handleRouteChange(routes.Home)}>
+              <i className='fa fa-home' />
+              &nbsp; Home
+            </Menu.Item>
+        ) : null}
+        {shouldShowRoute('Dashboard') ? (
+            <Menu.Item key='1' onClick={() => handleRouteChange(routes.Dashboard)}>
+              <i className='fa fa-home' />
+              &nbsp; Dashboard
+            </Menu.Item>
+        ) : null}
+        {shouldShowRoute('ContentCreatorDashboard') ? (
+            <Menu.Item
+                key='2'
+                onClick={() => handleRouteChange(routes.ContentCreatorDashboard)}
+            >
+              <i className='fa fa-home' />
+              &nbsp; Dashboard
+            </Menu.Item>
+        ) : null}
+        {shouldShowRoute('ResearcherDashboard') ? (
+            <Menu.Item
+                key='3'
+                onClick={() => handleRouteChange(routes.ResearcherDashboard)}
+            >
+              <i className='fa fa-home' />
+              &nbsp; Dashboard
+            </Menu.Item>
+        ) : null}
+        {shouldShowRoute('Sandbox') ? (
+            <Menu.Item
+                key='4'
+                onClick={() => {
+                  localStorage.removeItem('sandbox-activity');
+                  handleRouteChange(routes.Sandbox);
+                }}
+            >
+              <i className='fa fa-window-maximize' />
+              &nbsp; Sandbox
+            </Menu.Item>
+        ) : null}
+        {shouldShowRoute('Sandbox') ? (
+            <Menu.Item
+                key='9'
+                onClick={() => {
+                  localStorage.removeItem('sandbox-activity');
+                  handleRouteChange(routes.Gallery);
+                }}
+            >
+              <i className='fa fa-window-maximize' />
+              &nbsp; Gallery
+            </Menu.Item>
+        ) : null}
+        {shouldShowRoute('TeacherLogin') ? (
+            <Menu.Item
+                key='5'
+                onClick={() => handleRouteChange(routes.TeacherLogin)}
+            >
+              <i className='fa fa-sign-in-alt' />
+              &nbsp; User Login
+            </Menu.Item>
+        ) : null}
+        {shouldShowRoute('About') ? (
+            <Menu.Item key='6' onClick={() => handleRouteChange(routes.About)}>
+              <i className='fa fa-info-circle' />
+              &nbsp; About
+            </Menu.Item>
+        ) : null}
+        {shouldShowRoute('BugReport') ? (
+            <Menu.Item key='7' onClick={() => handleRouteChange(routes.BugReport)}>
+              <i className='fa fa-calendar-times' />
+              &nbsp; Report a Bug
+            </Menu.Item>
+        ) : null}
+        {shouldShowRoute('SignOut') ? (
+            <Menu.Item key='8' onClick={() => handleLogout()}>
+              <i className='fa fa-sign-out-alt' />
+              &nbsp; Sign Out
+            </Menu.Item>
+        ) : null}
+      </Menu>
   );
 
   return (
-    <span id='navBar'>
+      <span id='navBar'>
       <Link
-        id='link'
-        to={
-          value.role === 'ContentCreator'
-            ? '/ccdashboard'
-            : value.role === 'Mentor'
-            ? '/dashboard'
-            : value.role === 'Student'
-            ? '/student'
-            : value.role === 'Researcher'
-            ? '/report'
-            : '/'
-        }
+          id='link'
+          to={
+            value.role === 'ContentCreator'
+                ? '/ccdashboard'
+                : value.role === 'Mentor'
+                    ? '/dashboard'
+                    : value.role === 'Student'
+                        ? '/student'
+                        : value.role === 'Researcher'
+                            ? '/report'
+                            : '/'
+          }
       >
         <img src={Logo} id='casmm-logo' alt='logo' />
       </Link>
       <div id='dropdown-menu'>
         <Dropdown overlay={menu} trigger={['click']}>
           <button
-            className='ant-dropdown-link'
-            onClick={(e) => e.preventDefault()}
+              className='ant-dropdown-link'
+              onClick={(e) => e.preventDefault()}
           >
             {value.name ? value.name : 'Menu'} <DownOutlined />
           </button>
