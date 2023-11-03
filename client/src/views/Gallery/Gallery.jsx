31ae8abf28239d580af69f9fdb588655dd180165
Max Plummer <mplummer1@ufl.edu>
2023-11-03 14:08:58 -0400

    something else

---
+++ client/src/views/Gallery/Gallery.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import NSF from "../../assets/nsf_logo.png"
+import TAMU from "../../assets/tamu_logo.png"
+import UF from "../../assets/uf_logo.png"
+import NavBar from "../../components/NavBar/NavBar"
+import "./gallery.less"
+
+export default function Gallery(props) {
+    return (
+        <div className="container nav-padding">
+            <NavBar />
+            <div id="about-content-container">
+
+            </div>
+        </div>
+    )
+}
