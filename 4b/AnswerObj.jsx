8fc18599fb9ad33c818121a3ca7975ee3d08927f
JustinBeauchesne <beauchesnej@ufl.edu>
2023-11-03 13:56:33 -0400

    Format Answer Template

---
+++ 4b/AnswerObj.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+class Answer extends React.Component{
+    var isAnswer; // boolean
+    var answer; // string of answer
+    var aImage= null; // image for answer if needed
+    // answer display stuff
+    
+
+
+}
--- 4b/QuestionObj.jsx
+++ 4b/QuestionObj.jsx
@@ -1,8 +1,13 @@
 import React,{Component} from "react";
+import Answer from "./AnswerObj"
 class Question extends React.Component{
-    var type;
-    list <answer objects>
-    ...
+    var type; // string or state/enum
+    list <Answer> answers; // list of answer componets for each question store locally in each object
+    var question; // String holding question
+    var qImage = null; // Image to show in question if needed
 
+    // function that adds new answer
+
+    //function that removes existing answer
 
 }
