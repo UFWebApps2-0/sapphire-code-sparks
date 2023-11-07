6222893780741dee713c14addc346a910b4d1e3b
FrancoiseHayek <hayekfreelancing@gmail.com>
2023-11-07 12:01:15 -0500

    Embed Link regex checker
    Add a regex check to ensure that embed link starts and ends with an iframe tag

--- client/src/views/Mentor/Classroom/Home/VideoURL_Input.jsx
+++ client/src/views/Mentor/Classroom/Home/VideoURL_Input.jsx
@@ -5,11 +5,23 @@ function VideoURL_Input () {
 
   const [URL, setURL] = useState("");
 
+  const checkEmbedLink = URL => {
+    const regex = /<iframe.*?<\/iframe>/s;
+
+    if (URL.search(regex) === -1) {
+      return null;
+    }
+    return URL;
+  }
+
   return (
     <div>
       <Form.Item id="form-label" label="Video embed link:">
         <Input.TextArea
-          onChange={e => setURL(e.target.value)}
+          onChange={e => {
+            setURL(e.target.value)
+            console.log(checkEmbedLink(e.target.value))
+          }}
           value={URL}
           className="input"
           placeholder="Enter video embed link"
