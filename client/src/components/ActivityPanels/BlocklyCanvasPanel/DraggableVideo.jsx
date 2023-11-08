2cc947932e3d81130633843b864b39aa42d69e6f
CeciliaDurst <cecilia.durst@gmail.com>
2023-11-08 12:37:23 -0500

    Commenting and css

--- client/src/components/ActivityPanels/BlocklyCanvasPanel/DraggableVideo.jsx
+++ client/src/components/ActivityPanels/BlocklyCanvasPanel/DraggableVideo.jsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
 import '../ActivityLevels.less';
-import {CloseOutlined} from '@ant-design/icons-svg';
 
 export default function DraggableVideo(props) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     const [mouseDown, setMouseDown] = useState(false);
-    const {name, videoId} = props;
+    const {name, videoId} = props; //Video name and link are passed in 
 
+    //Draggable div reference: https://www.w3schools.com/howto/howto_js_draggable.asp
     const handleMouseDown = (e) => {
         setMouseDown(true);
         e.preventDefault();
@@ -35,54 +35,14 @@ export default function DraggableVideo(props) {
         document.onmousemove = null;
     }
 
-    // function dragElement(elmnt) {
-    //     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    //     if (document.getElementById(elmnt.id + "header")) {
-    //       // if present, the header is where you move the DIV from:
-    //       document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    //     } else {
-    //       // otherwise, move the DIV from anywhere inside the DIV:
-    //       elmnt.onmousedown = dragMouseDown;
-    //     }
-      
-    //     function dragMouseDown(e) {
-    //       e = e || window.event;
-    //       e.preventDefault();
-    //       // get the mouse cursor position at startup:
-    //       pos3 = e.clientX;
-    //       pos4 = e.clientY;
-    //       document.onmouseup = closeDragElement;
-    //       // call a function whenever the cursor moves:
-    //       document.onmousemove = elementDrag;
-    //     }
-      
-    //     function elementDrag(e) {
-    //       e = e || window.event;
-    //       e.preventDefault();
-    //       // calculate the new cursor position:
-    //       pos1 = pos3 - e.clientX;
-    //       pos2 = pos4 - e.clientY;
-    //       pos3 = e.clientX;
-    //       pos4 = e.clientY;
-    //       // set the element's new position:
-    //       elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    //       elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    //     }
-      
-    //     function closeDragElement() {
-    //       // stop moving when mouse button is released:
-    //       document.onmouseup = null;
-    //       document.onmousemove = null;
-    //     }
-    //   }
-    return (
+    return ( //Returns an iframe video wrapped in a div that will be used to drag the iframe around the workspace
         <div id = "draggableVideo"
+        //Draggability is currently broken
         onMouseDown = {event => handleMouseDown(event)}
         onMouseUp = {handleMouseUp}
         onMouseMove = {event => handleMouseMove(event, document.getElementById("draggableVideo"))}
         >
         <h2 id="vidTitle">{name} Video 
-        {/* <CloseOutlined /> */}
         </h2>
         <iframe 
         width="280" height="200"
