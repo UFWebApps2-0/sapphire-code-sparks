e4040082b7d27c70c9d6fc55f728b15dc5e31afd
Nathan Wand <nathan.wand1234@gmail.com>
2023-11-13 14:08:51 -0500

    connected EmailJs with the FlagButton

--- client/src/components/FlagButton/FlagButton.jsx
+++ client/src/components/FlagButton/FlagButton.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 import './FlagButton.less';
 import Flag from  "../../assets/flag.png";
 
 export default function FlagButton({ id }) {
   const [highlighted, setHighlighted] = useState(false);
   const [message, setMessage] = useState({words:0});
-
+  emailjs.init('SM7VwO6yrGjIp_Z_M');
+  
+  function sendEmail() {
+    emailjs.send("service_15kdnle","template_b73zh4s",{
+      video_id: id,
+    });
+  }
+  
   const handleClick = () => {
     setHighlighted(!highlighted);
+    sendEmail();
   }
 
   return (
@@ -20,3 +29,4 @@ export default function FlagButton({ id }) {
     </button>
   );
 }
+
