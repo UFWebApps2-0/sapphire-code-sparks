d37a4ad208d49f5141e280a09b50b61c2b5199ef
Nathan Wand <nathan.wand1234@gmail.com>
2023-11-13 14:25:19 -0500

    added comments and better formatting

--- client/src/components/FlagButton/FlagButton.jsx
+++ client/src/components/FlagButton/FlagButton.jsx
@@ -1,32 +1,44 @@
-import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
-import './FlagButton.less';
-import Flag from  "../../assets/flag.png";
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import "./FlagButton.less";
+import Flag from "../../assets/flag.png";
 
 export default function FlagButton({ id }) {
+  // State for button highlight and message visibility
   const [highlighted, setHighlighted] = useState(false);
-  const [message, setMessage] = useState({words:0});
-  emailjs.init('SM7VwO6yrGjIp_Z_M');
-  
+  const [message, setMessage] = useState({ words: 0 });
+
+  // Initialize EmailJS
+  emailjs.init("SM7VwO6yrGjIp_Z_M");
+
+  // Function to send email
   function sendEmail() {
-    emailjs.send("service_15kdnle","template_b73zh4s",{
+    emailjs.send("service_15kdnle", "template_b73zh4s", {
       video_id: id,
     });
   }
-  
+
+  // Handle button click
   const handleClick = () => {
     setHighlighted(!highlighted);
     sendEmail();
-  }
+  };
 
+  // Button component
   return (
-    <button id="flag-button" className={highlighted ? 'highlighted' : ''} type='button' onClick={handleClick} disabled={highlighted}
-    onMouseEnter = {e => setMessage({words:1})} onMouseLeave = {e => setMessage({words:0})}
+    <button
+      id="flag-button"
+      className={highlighted ? "highlighted" : ""}
+      type="button"
+      onClick={handleClick}
+      disabled={highlighted}
+      onMouseEnter={(e) => setMessage({ words: 1 })}
+      onMouseLeave={(e) => setMessage({ words: 0 })}
     >
       <img src={Flag} alt="Flag" />
-      <div id = "message" style = {{opacity: message.words}}>Report video</div>
-      
+      <div id="message" style={{ opacity: message.words }}>
+        Report video
+      </div>
     </button>
   );
 }
-
