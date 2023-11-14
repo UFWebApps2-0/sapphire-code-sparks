81b995fbe91794aa20e6292fede0491554d4c024
Max Plummer <mplummer1@ufl.edu>
2023-11-14 17:56:43 -0500

    Added copy/edit icons

--- client/src/components/GalleryView/GalleryView.jsx
+++ client/src/components/GalleryView/GalleryView.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { HeartOutlined, HeartFilled } from '@ant-design/icons';
+import { HeartOutlined, HeartFilled, CopyOutlined, EditOutlined } from '@ant-design/icons';
 import './GalleryView.less';
 import DemoData from "../../../DemoData.json"
 
@@ -11,7 +11,10 @@ export default function GalleryView({searchParams, setSearchParams, filterText, 
     const [page, setPage] = useState(
         searchParams.has('page') ? parseInt(searchParams.get('page')) : 1
     );
-    const [Icon, setIcon] = useState(HeartOutlined);
+    const [HeartIcon, setHeartIcon] = useState(HeartOutlined);
+    const [CopyIcon, setCopyIcon] = useState(CopyOutlined);
+    const [EditIcon, setEditIcon] = useState(EditOutlined);
+
 
 
     const handleOpenGallery = (id) => {
@@ -20,11 +23,15 @@ export default function GalleryView({searchParams, setSearchParams, filterText, 
 
     const handleLike = (id) => {
         alert("Workspace will be liked");
-        if (Icon === HeartOutlined)
-            setIcon(HeartFilled);
+        if (HeartIcon === HeartOutlined)
+            setHeartIcon(HeartFilled);
         else
-            setIcon(HeartOutlined);
+            setHeartIcon(HeartOutlined);
     };
+
+    const handleCopyEdit = (id) => {
+        alert("Workspace will be copied/edited");
+    }
 
 
     // Set workspaceList with the entries from JSON data and filter for privacy setting
@@ -37,6 +44,7 @@ export default function GalleryView({searchParams, setSearchParams, filterText, 
         entry.name.toLowerCase().includes(filterText.toLowerCase()));
 
     // The list is displayed as cards and filters as input is typed in the search bar
+    // TODO: Check margins if format looks weird
     const galleryList = filteredGallery.map(directory => {
         return (
             <div key={directory.id} id='gallery-class-card'>
@@ -50,10 +58,14 @@ export default function GalleryView({searchParams, setSearchParams, filterText, 
                     </div>
                 </div>
                 <div id='card-right-content-container'>
-                    <button id='likeButton' onClick={() => handleLike(DemoData.id)}>
-                        <Icon size={64}/>
+                    <button id='likeButton' style={{marginBottom: '0vh'}} onClick={() => handleLike(DemoData.id)}>
+                        <HeartIcon size={64}/>
                     </button>
-                    <div id='divider' />
+                    <button id='copy-edit-button' onClick={() => handleCopyEdit(DemoData.id)}>
+                        <CopyIcon size={64} style={{marginTop: '0vh'}}/>/
+                        <EditIcon size={64} style={{marginTop: '0vh'}}/>
+                    </button>
+                    <div id='divider' style={{marginTop: '0vh', marginBottom: '2vh'}}/>
                     <div id='student-number-container'>
                         <h1 id='number'>0</h1>
                         <p id='label'>Views</p>
