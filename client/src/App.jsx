c05cab683c312899d33086baab2eb831a7f82d86
sambattis <123843507+sambattis@users.noreply.github.com>
2023-11-24 01:45:48 -0500

    initial integration preview assessment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ import ForgetPassword from "./views/TeacherLogin/ForgetPassword";
 import ResetPassword from "./views/TeacherLogin/ResetPassword";
 import TeacherLogin from "./views/TeacherLogin/TeacherLogin";
 import TeacherViewAssessments from "../../4b/TeacherViewAssessments";
+import SpecificAssessmentPreview from '../../4b/SpecificAssessmentPreview';
 
 const App = () => {
   return (
@@ -56,6 +57,14 @@ const App = () => {
                 <StudentViewAssessments assessmentList = {null}/> // null is placeholder for database info
             }
         />
+
+      <Route
+            path='/assessment-preview'
+            element={
+                <SpecificAssessmentPreview/> // null is placeholder for database info
+            }
+        />
+        
 
         <Route
           path="/report"
---
+++ client/src/components/AssessPreview/AssignToClassroomButton.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState} from 'react'
+import { Link } from 'react-router-dom'
+
+const AssignToClassroomButton = () => {
+    return (
+                <Link to = "/">
+                    <button>Assign to Classroom</button>
+                </Link>
+        
+    )
+}
+
+
+
+
+export default AssignToClassroomButton
