007205a5a39544f0f19a11adc223febb8e5bfb6d
CeciliaDurst <cecilia.durst@gmail.com>
2023-11-27 15:53:48 -0500

    Querying

--- client/src/components/ActivityPanels/BlocklyCanvasPanel/DraggableVideo.jsx
+++ client/src/components/ActivityPanels/BlocklyCanvasPanel/DraggableVideo.jsx
@@ -1,29 +1,44 @@
 import React, {useState} from 'react';
 import '../ActivityLevels.less';
 import FlagButton from '../../FlagButton/FlagButton.jsx';
+import { getVideoLink} from '../../../Utils/requests';
 
 export default function DraggableVideo(props) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    const [pos1, setPos1] = useState(0);
+    const [pos2, setPos2] = useState(0); 
+    const [pos3, setPos3] = useState(0); 
+    const [pos4, setPos4] = useState(0); 
+ 
     const [mouseDown, setMouseDown] = useState(false);
     const {name, videoId} = props; //Video name and link are passed in 
 
     //Draggable div reference: https://www.w3schools.com/howto/howto_js_draggable.asp
+    //New Ref?? https://stackoverflow.com/questions/20926551/recommended-way-of-making-react-component-div-draggable 
     const handleMouseDown = (e) => {
         setMouseDown(true);
         e.preventDefault();
         // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
+        setPos3(e.clientX);
+        setPos4(e.clientY);
+        console.log("Mousedown pos 3: "+pos3);
+        // document.onmouseup = handleMouseUp;
+        // document.onmousemove = handleMouseMove(e);
       };
 
     const handleMouseMove = (e, elmnt) => {
+        //alert("Working");
+        //e = e;
         e.preventDefault();
         if(mouseDown){
+            //alert("Mousedown")
             // calculate the new cursor position:
-            pos1 = pos3 - e.clientX;
-            pos2 = pos4 - e.clientY;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
+            console.log("Moving pos 3: "+pos3);
+
+            setPos1(pos3 - e.clientX);
+            setPos2(pos4 - e.clientY);
+            setPos3(e.clientX);
+            setPos4(e.clientY);
+            console.log("Moving pos 1: "+pos1);
             // set the element's new position:
             elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
             elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
@@ -31,10 +46,25 @@ export default function DraggableVideo(props) {
     }
 
     const handleMouseUp = () => {
+        console.log("mouseup");
         setMouseDown(false);
         document.onmouseup = null;
         document.onmousemove = null;
     }
+
+    const getLink = (id) => {
+        getVideoLink(id).then((res) => {
+            if (res.data) {
+                console.log("URL: " + res.data.URL);
+              return res.data.URL;
+            
+            } else {
+              console.log("No video");
+            }
+          });
+    }
+    const vidLink = getLink(2);
+
 
     return ( //Returns an iframe video wrapped in a div that will be used to drag the iframe around the workspace
         <div id = "draggableVideo"
@@ -46,9 +76,12 @@ export default function DraggableVideo(props) {
         <h2 id="vidTitle">{name} Video <FlagButton id = {1}/>
         </h2>
         
+        {/* Retrieve video link */}
+        
+
         <iframe 
         width="280" height="200"
-        src={videoId} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen
+        src={vidLink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen
         />
         </div>
     );
