1ce18a5a9324b07d21902beebf0f63480d42249d
Gio <44237862+Redifexx@users.noreply.github.com>
2023-11-28 13:15:11 -0500

    added some stuff to dashboard

--- client/src/views/Student/Dashboard.jsx
+++ client/src/views/Student/Dashboard.jsx
@@ -2,7 +2,7 @@ import { message } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
-import { getStudentName } from '../../Utils/requests';
+import { getStudentName, getStudentClassroom } from '../../Utils/requests';
 import './Student.less';
 
 export default function Dashboard(){
@@ -14,7 +14,6 @@ export default function Dashboard(){
         const fetchData = async () => {
         try {
             const res = await getStudentName();
-            console.log(JSON.stringify(res.data.students[0].name));
             if (res.data) {
             if (res.data.students) {
                 setCurrentStudent(res.data.students[0].name);
@@ -26,6 +25,29 @@ export default function Dashboard(){
         };
         fetchData();
     }, []);
+
+    useEffect(() => {
+        const fetchData = async () => {
+        try {
+            const res = await getStudentClassroom();
+            if (res.data) {
+                console.log(JSON.stringify(res.data.lesson_module.name));
+                if (res.data.lesson_module)
+                {
+                    setLessonModule(res.data.lesson_module);
+                }
+            } else {
+            message.error(res.err);
+            }
+        } catch {}
+        };
+        fetchData();
+    }, []);
+
+    const handleLessonButton = () => {
+        navigate('/studentLessons')
+      };
+
     return(
         <div className='container nav-padding'>
             <NavBar />
@@ -33,16 +55,16 @@ export default function Dashboard(){
                 <div id='header'>
                     <div>Dashboard</div>
                 </div>
-                <h1>Welcome {currentStudent} !</h1>
+                <h1 id="studentNameHeader">Welcome {typeof currentStudent === 'string' ? JSON.stringify(currentStudent).slice(1, -1) : ''} !</h1>
+                <h2 id="lessonModuleHeader">Current Lesson Module</h2>
                 <div id="containerDashboard">
-                    <div className="dashboard-box">
-                        Project 1       
-                    </div>
-                    <div className="dashboard-box">
-                        Project 2
-                    </div>
-                    <div className="dashboard-box">
-                        Project 3
+                    <div className="dashboard-box" onClick={handleLessonButton}>
+                        <div id="lessonName">
+                        {typeof learningStandard.name === 'string' ? JSON.stringify(learningStandard.name).slice(1, -1) : ''}
+                        </div>
+                        <div>
+                        {typeof learningStandard.expectations === 'string' ? JSON.stringify(learningStandard.expectations).slice(1, -1) : ''}
+                        </div>
                     </div>
                 </div>
             </div>
