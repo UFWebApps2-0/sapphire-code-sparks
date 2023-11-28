a01ef655730c6302da4f93930caebc79897d1744
Austin Rhee <austin_rhee@comcast.net>
2023-11-27 19:11:50 -0500

    Updated DisplayQuestion (Needs routing)

--- 4b/DisplayQuestion.jsx
+++ 4b/DisplayQuestion.jsx
@@ -1,99 +1,131 @@
-import "./AssessmentObj";
-import "./QuestionObj";
-import "./QuesitonList";
-import React from "react";
-import { RadioGroup } from 'react-bootstrap';
-import Editable from "react-editable-title";
-import "../client/src/assets/style.less";
+import React, { Component } from "react";
+import "./QuestionFormStyle.css";
 
-function displayQuestion( { assesment } ) {
-    const [data, setData] = React.useState(assesment);
-    const [question, setQuestion] = React.useState(assesment.questions);
 
-    function handleSubmission() {
-        // TODO: Handle form
+class DisplayQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            responses: {},
+        };
+    }
+    
+    handleChange = (question, value) => {
+        this.setState((prevState) => ({
+            responses: {
+                ...prevState.responses,
+                [question]: value,
+            },
+        }));
     };
 
-    function handleChange() {
-        // TODO: Handle question input
-    };
+    handleSubmit = (e) => {
+        const { assessment } = this.props;
+        const { responses } = this.state;
+        const questions = assessment.questions
 
-    function questionRender(q) {
-        if(q.type in q.TYPES) {
-            switch (q.type) {
-                case ('multipleChoice'): {
-                    return (
-                        <div onChange={handleChange}>
-                            {q.choices.map((c, index) => (
-                                <input type="radio" value={c}></input>
-                            ))}
-                        </div>
-                    );
-                }
-
-                case ('multiSelect'): {
-                    return (
-                        <div onChange={handleChange}>
-                            {q.choices.map((c, index) => (
-                                <input type=""></input>
-                            ))}
-                        </div>
-                    );
-                }
-
-                case ('dropDown'): {
-                    return (
-                        <div onChange={handleChange}>
-                            {q.choices.map((c, index) => (
-                                <input type=""></input>
-                            ))}
-                        </div>
-                    );
-                }
-
-                case ('freeResponse'): {
-                    return (
-                        <div onChange={handleChange}>
-                            <input> </input>
-                        </div>
-                    );
-                }
-
-                case ('codingQ'): {
-                    // Call existing code workspace
-                    break;
-                }
-            }
+        e.preventDefault();
+        const allAnswered = questions.every((question) => responses.hasOwnProperty(question.prompt));
+        if (allAnswered) {
+            console.log(this.state.responses);
+            // Call database here
+            this.setState({responses: {}});
         }
+        else {
+            alert('Please answer all questions.')
+        }
+    };
+
+    renderQuestions = () => {
+        const { assessment } = this.props;
+        const { responses } = this.state;
+        const questions = assessment.questions
+
+        return questions.map((question, index) => {
+            const questionNumber = index + 1;
+            switch (question.type) {
+                case 'multipleChoice':
+                    return (
+                        <div className="form-section" key={question.prompt}>
+                            <span>
+                                <strong> {questionNumber}. </strong>
+                                {question.prompt}
+                            </span>
+                            <ul>
+                                {question.choices.map((choice, index) => (
+                                    <li key={index}>
+                                        <label>
+                                            <input
+                                                type="radio"
+                                                value={choice}
+                                                checked={responses[question.prompt] === choice}
+                                                onChange={() => this.handleChange(question.prompt, choice)}
+                                            />
+                                            {choice}
+                                        </label>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    );
+                case 'dropDown':
+                    return (
+                        <div className="form-section" key={question.prompt}>
+                            <span>
+                                <strong> {questionNumber}. </strong>
+                                {question.prompt}
+                            </span>
+                            <div>
+                                <select
+                                    value={responses[question.prompt] || ' '}
+                                    onChange={(e) => this.handleChange(question.prompt, e.target.value)}
+                                >
+                                    <option value="">Choose</option>
+                                    {question.choices.map((choice, index) => (
+                                        <option key={index} value={choice}>
+                                            {choice}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
+                    );
+                case 'freeResponse':
+                    return (
+                        <div className="form-section space-between" key={question.prompt}>
+                            <span>
+                                <strong> {questionNumber}. </strong>
+                                {question.prompt}
+                            </span>
+                            <div className="space-between">
+                            <textarea
+                                value={responses[question.prompt] || ' '}
+                                onChange={(e) => this.handleChange(question.prompt, e.target.value)}
+                            />
+                            </div>
+                        </div>
+                    )
+            }
+        })
     }
 
-    const questionForm = ( { question } ) => {
+    render() {
         return (
-            <form>
-                {question.map((q, index) => (
-                    <div key={index}>
-                        <label>{q.prompt}</label>
-                        {questionRender(q)}
-                    </div>
-                ))}
+        
+        <div className="tableMid">
+            <form
+            onSubmit={(e) => this.handleSubmit(e)}
+            className="flex-container flex-column"
+            >
+                <div className="form-section">
+                    {this.renderQuestions()}
+                    <button type="button" onClick={(e) => this.handleSubmit(e)}>Submit</button>
+                </div>
             </form>
-        );
-    };
-
-    // return (
-    //     <div>
-    //         {}
-    //         <div className="row">
-    //             <div className="col">
-    //                 <h1>{this.data.name}</h1>
-    //                 <h2>{this.question.length} Questions</h2>
-    //             </div>
-    //             <form id="mainForm" onSubmit={handleSubmission}>
-                    
-
-    //             </form>
-    //         </div>
-    //     </div>
-    // );
+        </div>
+        )
+    }
 
 }
+
+export default DisplayQuestion
