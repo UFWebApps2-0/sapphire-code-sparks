a18cf1ab59d6d2d9d0628d316bc73b809b623b63
lnsidiou5 <21jason.li@gmail.com>
2023-11-28 20:17:45 -0500

    Connect to database and fixed routing.

--- client/src/components/AssessPreview/EditAssessmentButton.jsx
+++ client/src/components/AssessPreview/EditAssessmentButton.jsx
@@ -1,22 +1,15 @@
-import { useEffect, useState} from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
+const doThing = () => {};
+function EditAssessmentButton({ id }) {
+  const act = <Link to="/teacher-assessments/editor/"></Link>; //does ":" stay?
 
-const doThing = () => {
-    
-}
-function EditAssessmentButton ({id}) {
-    
-    const act = <Link to="/teacher-assessments/editor/:"></Link>; //does ":" stay?
-    
-    return (
-                <Link to = {"/teacher-assessments/editor/:"+ id}>
-                    <button onSubmit = {doThing}>Edit</button >
-                </Link>
-    )
+  return (
+    <Link to={"/teacher-assessments/editor/" + id}>
+      <button onSubmit={doThing}>Edit</button>
+    </Link>
+  );
 }
 
-
-
-
-export default EditAssessmentButton
+export default EditAssessmentButton;
