894b4c0ec2e583640e8671c41e56c87b4846a984
lnsidiou5 <21jason.li@gmail.com>
2023-11-30 17:18:37 -0500

    Update display question styling

--- client/src/views/DisplayQuestion/DisplayQuestion.jsx
+++ client/src/views/DisplayQuestion/DisplayQuestion.jsx
@@ -2,156 +2,164 @@ import React, { Component } from "react";
 import "./QuestionFormStyles.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-
 class DisplayQuestion extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            responses: {},
-        };
-    }
-    
-    handleChange = (question, value) => {
-        this.setState((prevState) => ({
-            responses: {
-                ...prevState.responses,
-                [question]: value,
-            },
-        }));
+  constructor(props) {
+    super(props);
+    this.state = {
+      responses: {},
     };
+  }
 
-    handleSubmit = (e) => {
-        const { assessment } = this.props;
-        const { responses } = this.state;
-        const questions = assessment.questions
+  handleChange = (question, value) => {
+    this.setState((prevState) => ({
+      responses: {
+        ...prevState.responses,
+        [question]: value,
+      },
+    }));
+  };
 
-        e.preventDefault();
-        const allAnswered = questions.every((question) => responses.hasOwnProperty(question.prompt));
-        if (allAnswered) {
-            console.log(this.state.responses);
-            // Call database here
-            this.setState({responses: {}});
-        }
-        else {
-            alert('Please answer all questions.')
-        }
-    };
+  handleSubmit = (e) => {
+    const { assessment } = this.props;
+    const { responses } = this.state;
+    const questions = assessment.questions;
 
-    renderQuestions = () => {
-        const { assessment } = this.props;
-        const { responses } = this.state;
-        const questions = assessment.questions
-
-        return questions.map((question, index) => {
-            const questionNumber = index + 1;
-            switch (question.type) {
-                case 'multipleChoice':
-                    return (
-                        <div className="form-section" key={question.prompt}>
-                            <span>
-                                <strong> {questionNumber}. </strong>
-                                {question.prompt}
-                            </span>
-                            <ul>
-                                {question.choices.map((choice, index) => (
-                                    <li key={index}>
-                                        <label>
-                                            <input
-                                                type="radio"
-                                                value={choice}
-                                                checked={responses[question.prompt] === choice}
-                                                onChange={() => this.handleChange(question.prompt, choice)}
-                                            />
-                                            {choice}
-                                        </label>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    );
-                    case 'multiSelect':
-                        return (
-                            <div className="form-section" key={question.prompt}>
-                                <span>
-                                    <strong> {questionNumber}. </strong>
-                                    {question.prompt}
-                                </span>
-                                <ul>
-                                    {question.choices.map((choice, index) => (
-                                        <li key={index}>
-                                            <label>
-                                                <input
-                                                    type="checkbox"
-                                                    value={choice}
-                                                    checked={responses[question.prompt]?.includes(choice)}
-                                                    onChange={() => this.handleChange(question.prompt, choice)}
-                                                />
-                                                {choice}
-                                            </label>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        );
-                
-                case 'dropDown':
-                    return (
-                        <div className="form-section" key={question.prompt}>
-                            <span>
-                                <strong> {questionNumber}. </strong>
-                                {question.prompt}
-                            </span>
-                            <div>
-                                <select
-                                    value={responses[question.prompt] || ' '}
-                                    onChange={(e) => this.handleChange(question.prompt, e.target.value)}
-                                >
-                                    <option value="">Choose</option>
-                                    {question.choices.map((choice, index) => (
-                                        <option key={index} value={choice}>
-                                            {choice}
-                                        </option>
-                                    ))}
-                                </select>
-                            </div>
-                        </div>
-                    );
-                case 'freeResponse':
-                    return (
-                        <div className="form-section space-between" key={question.prompt}>
-                            <span>
-                                <strong> {questionNumber}. </strong>
-                                {question.prompt}
-                            </span>
-                            <div className="space-between">
-                            <textarea
-                                value={responses[question.prompt] || ' '}
-                                onChange={(e) => this.handleChange(question.prompt, e.target.value)}
-                            />
-                            </div>
-                        </div>
-                    )
-            }
-        })
+    e.preventDefault();
+    const allAnswered = questions.every((question) =>
+      responses.hasOwnProperty(question.prompt)
+    );
+    if (allAnswered) {
+      console.log(this.state.responses);
+      // Call database here
+      this.setState({ responses: {} });
+    } else {
+      alert("Please answer all questions.");
     }
+  };
 
-    render() {
-        return (
-        
-        <div className="tableMid">
-            <form
-            onSubmit={(e) => this.handleSubmit(e)}
-            className="flex-container flex-column"
-            >
-                <div className="form-section">
-                    {this.renderQuestions()}
-                    <button type="button" onClick={(e) => this.handleSubmit(e)}>Submit</button>
-                </div>
-            </form>
-        </div>
-        )
-    }
+  renderQuestions = () => {
+    const { assessment } = this.props;
+    const { responses } = this.state;
+    const questions = assessment.questions;
 
+    return questions.map((question, index) => {
+      const questionNumber = index + 1;
+      switch (question.type) {
+        case "multipleChoice":
+          return (
+            <div className="form-section" key={question.prompt}>
+              <span>
+                <strong> {questionNumber}. </strong>
+                {question.prompt}
+              </span>
+              <ul>
+                {question.choices.map((choice, index) => (
+                  <li key={index}>
+                    <label>
+                      <input
+                        type="radio"
+                        value={choice}
+                        checked={responses[question.prompt] === choice}
+                        onChange={() =>
+                          this.handleChange(question.prompt, choice)
+                        }
+                      />
+                      {choice}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          );
+        case "multiSelect":
+          return (
+            <div className="form-section" key={question.prompt}>
+              <span>
+                <strong> {questionNumber}. </strong>
+                {question.prompt}
+              </span>
+              <ul>
+                {question.choices.map((choice, index) => (
+                  <li key={index}>
+                    <label>
+                      <input
+                        type="checkbox"
+                        value={choice}
+                        checked={responses[question.prompt]?.includes(choice)}
+                        onChange={() =>
+                          this.handleChange(question.prompt, choice)
+                        }
+                      />
+                      {choice}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          );
+
+        case "dropDown":
+          return (
+            <div className="form-section" key={question.prompt}>
+              <span>
+                <strong> {questionNumber}. </strong>
+                {question.prompt}
+              </span>
+              <div>
+                <select
+                  value={responses[question.prompt] || " "}
+                  onChange={(e) =>
+                    this.handleChange(question.prompt, e.target.value)
+                  }
+                >
+                  <option value="">Choose</option>
+                  {question.choices.map((choice, index) => (
+                    <option key={index} value={choice}>
+                      {choice}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          );
+        case "freeResponse":
+          return (
+            <div className="form-section space-between" key={question.prompt}>
+              <span>
+                <strong> {questionNumber}. </strong>
+                {question.prompt}
+              </span>
+              <div className="space-between">
+                <textarea
+                  value={responses[question.prompt] || " "}
+                  onChange={(e) =>
+                    this.handleChange(question.prompt, e.target.value)
+                  }
+                />
+              </div>
+            </div>
+          );
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="tableMid" style={{ border: 0 }}>
+        <form
+          onSubmit={(e) => this.handleSubmit(e)}
+          className="flex-container flex-column"
+        >
+          <div className="form-section">
+            {this.renderQuestions()}
+            <button type="button" onClick={(e) => this.handleSubmit(e)}>
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default DisplayQuestion
+export default DisplayQuestion;
--- client/src/views/DisplayQuestion/DisplayQuestionView.jsx
+++ client/src/views/DisplayQuestion/DisplayQuestionView.jsx
@@ -5,69 +5,64 @@ import NavBar from "../../components/NavBar/NavBar";
 import { useNavigate, useParams } from "react-router-dom";
 import DisplayQuestion from "./DisplayQuestion";
 import Assessment from "../../Utils/AssessmentObj";
-import {
-  getAssessment,
-} from "../../Utils/requests";
+import { getAssessment } from "../../Utils/requests";
 
 export default function DisplayQuestionView() {
-    const [assessment, setAssessment] = useState(new Assessment());
-    const [responses, setResponses] = useState();
-    const navigate = useNavigate();
-    const { id } = useParams();
+  const [assessment, setAssessment] = useState(new Assessment());
+  const [responses, setResponses] = useState();
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-    useEffect(() => {
-        sessionStorage.setItem("assessmentId", id);
-        const fetchData = async () => {
-          try {
-            const res = await getAssessment(id);
-            return res;
-          } catch {
-            return { err: "Data fetch failed" };
-          }
-        };
-        fetchData().then((res) => {
-          if (res.data) {
-            setAssessment(
-              new Assessment({
-                ...res.data,
-                dueDate: res.data.dueDate,
-                openDate: res.data.openDate,
-                publishDate: res.data.publishDate,
-              })
-            );
-          } else {
-            message.error(res.err);
-            setAssessment({
-              name: "Assessment Not Found",
-            });
-          }
+  useEffect(() => {
+    sessionStorage.setItem("assessmentId", id);
+    const fetchData = async () => {
+      try {
+        const res = await getAssessment(id);
+        return res;
+      } catch {
+        return { err: "Data fetch failed" };
+      }
+    };
+    fetchData().then((res) => {
+      if (res.data) {
+        setAssessment(
+          new Assessment({
+            ...res.data,
+            dueDate: res.data.dueDate,
+            openDate: res.data.openDate,
+            publishDate: res.data.publishDate,
+          })
+        );
+      } else {
+        message.error(res.err);
+        setAssessment({
+          name: "Assessment Not Found",
         });
-      }, [id]);
+      }
+    });
+  }, [id]);
 
-      const handleViewAssessments = () => {
-        navigate(`/teacher-assessments`);
-      };
+  const handleViewAssessments = () => {
+    navigate(`/teacher-assessments`);
+  };
 
-    return (
-        <div className="container nav-padding">
-          <div id="main-header">{assessment.name}</div>
-          <NavBar />
-          <div id="classrooms-container">
-            <div className="container" >
-                <DisplayQuestion
-                assessment = {assessment}
-                responses = {setResponses}
-                />
-            </div>
-            <button onClick={() => {
-                const res = confirm("Leave assessment?");
-                if (res) {
-                navigate("/teacher-assessments");
-                }
-            }}>
-            Leave
-            </button>
-          </div>
-        </div>
-    );
+  return (
+    <div className="container nav-padding">
+      <div id="main-header">{assessment.name}</div>
+      <NavBar />
+      <div id="classrooms-container">
+        <DisplayQuestion assessment={assessment} responses={setResponses} />
+        <button
+          onClick={() => {
+            const res = confirm("Leave assessment?");
+            if (res) {
+              navigate("/teacher-assessments");
+            }
+          }}
+        >
+          Leave
+        </button>
+      </div>
+    </div>
+  );
 }
