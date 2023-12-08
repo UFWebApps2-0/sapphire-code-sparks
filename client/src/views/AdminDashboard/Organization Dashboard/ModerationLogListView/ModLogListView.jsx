30918799054cfaf9c399d002462300747bb96364
connorvoigt <connor.voigt@gmail.com>
2023-12-08 18:03:55 -0500

    Added ModLogListView folder+file for organization

---
+++ client/src/views/AdminDashboard/Organization Dashboard/ModerationLogListView/ModLogListView.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Table } from 'antd';
+import { QuestionCircleOutlined } from '@ant-design/icons';
+import { getOrganization, getModRecord, getModRecordCount } from "../../../Utils/requests";
+
+
+const getFormattedDate = (value, locale = 'en-US') => {
+  if(value == null) return "Never";
+  const date = new Date(value);
+  return date.toLocaleDateString(locale, {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+  });
+}
+
+
+export default function ModLogListView({data}) {
+    
+    const columns = [
+        { title: 'ID', dataIndex: 'id', key: 'id' },
+        { title: 'Action Type', dataIndex: 'ActionType', key: 'ActionType' },
+        { title: 'Action Date', dataIndex: 'ActionDate', key: 'ActionDate' },
+        { title: 'Moderator Name', dataIndex: 'ModeratorName', key: 'ModeratorName' },
+      ];
+
+
+      return (
+        <div id='table-container'>
+          <Table
+              columns={columns}
+              dataSource={data}
+              rowClassName='editable-row'
+              pagination={{
+              pageSizeOptions: ['10', '20', '30'],
+              showSizeChanger: true,
+              }}
+          />
+        </div>
+      );
+}
